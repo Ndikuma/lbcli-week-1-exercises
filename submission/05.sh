d2993573f274563166b7e965ddd48218af2a631f
Ndikumana <ndiku6241@gmail.com>
2026-03-14 17:10:55 +0200

    fixed during get balance  for add loading wallet before get his balance

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,6 @@
 # Check the total amount in the wallet.
-bitcoin-cli -regtest loadwallet builderswallet
-bitcoin-cli -regtest getbalance
+# Load the wallet (ignore warning if already loaded)
+bitcoin-cli -regtest loadwallet "builderswallet" 2>/dev/null > /dev/null
+
+# Get the wallet balance and output just the number
+bitcoin-cli -regtest -rpcwallet="builderswallet" getbalance
