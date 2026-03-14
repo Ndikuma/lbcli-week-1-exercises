1fd8bebb69940730577770180093f3bb103d6c4b
Ndikumana <ndiku6241@gmail.com>
2026-03-14 17:54:10 +0200

    fixed during get balance  for add loading wallet before get his balance

--- submission/05.sh
+++ submission/05.sh
@@ -1,9 +1,4 @@
 # Check the total amount in the wallet.
 #!/bin/bash
 
-ADDR=$(bitcoin-cli -regtest getnewaddress)
-
-bitcoin-cli -regtest generatetoaddress 10 "$ADDR" > /dev/null
-sleep 60
-
 bitcoin-cli -regtest getbalance
