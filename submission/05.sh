59a204a31f8035a484c35505dcea664321580588
Ndikumana <ndiku6241@gmail.com>
2026-03-14 17:50:45 +0200

    fixed during get balance  for add loading wallet before get his balance

--- submission/05.sh
+++ submission/05.sh
@@ -3,6 +3,7 @@
 
 ADDR=$(bitcoin-cli -regtest getnewaddress)
 
-bitcoin-cli -regtest generatetoaddress 1 "$ADDR" > /dev/null
+bitcoin-cli -regtest generatetoaddress 10 "$ADDR" > /dev/null
+sleep 60
 
 bitcoin-cli -regtest getbalance
