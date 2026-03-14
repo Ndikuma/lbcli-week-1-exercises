71464654c3ed227c76da46fd02cc3f6fc29b8078
Ndikumana <ndiku6241@gmail.com>
2026-03-14 17:03:24 +0200

    fixed during get networkname

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,3 @@
 # Write the bitcoin cli command to get the bitcoin node network name
-bitcoin-cli -regtest getblockchaininfo | grep '"chain"' 
+NODE_NETWORK=$(bitcoin-cli -regtest getblockchaininfo | grep '"chain"' | awk -F'"' '{print $4}')
+echo "$NODE_NETWORK"
